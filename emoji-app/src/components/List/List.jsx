216919a3553ff54648fbab0bc5bc07be99b1c884
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-06 13:40:25 +0100

    Add element using styled components

--- emoji-app/src/components/List/List.jsx
+++ emoji-app/src/components/List/List.jsx
@@ -1,11 +1,19 @@
 import { Item } from "../Item/Item";
 import styles from "./List.module.css";
+import styled from "styled-components";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
+
+const StyledButton = styled.button`
+  background: salmon;
+  color: white;
+  border: none;
+`;
 
 export function List() {
   return (
     <>
+      <StyledButton>Test</StyledButton>
       <button
         className={styles.btn}
         onClick={() => alert(`Liczba emoji: ${emojis.length}`)}
