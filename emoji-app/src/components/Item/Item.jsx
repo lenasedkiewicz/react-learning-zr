4b93affafc583274782f91008542580c7ab06dfa
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-06 14:17:59 +0100

    Destructuring on props

--- emoji-app/src/components/Item/Item.jsx
+++ emoji-app/src/components/Item/Item.jsx
@@ -14,7 +14,7 @@ const StyledEmoji = styled.span`
   margin-bottom: 42px;
   font-size: 32px;
   transition: transform 0.3s;
-  transform: ${(props) => (props.zoomed ? "scale(2)" : "scale(1)")};
+  transform: ${({ zoomed }) => (zoomed ? "scale(2)" : "scale(1)")};
 `;
 
 const StyledButton = styled.button`
