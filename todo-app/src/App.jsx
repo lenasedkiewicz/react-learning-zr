1cbc7e9b416633068ef2bff21a6e0d9ed29ea5a3
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-10 18:26:45 +0100

    Add header with styles

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className="">
+          <h1>Do zrobienia</h1>
+          <h2>5 zadań</h2>
+        </div>
+        <button className={styles.button}>+</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
