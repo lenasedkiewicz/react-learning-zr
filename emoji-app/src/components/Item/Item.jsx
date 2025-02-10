930eec1113d608e16c3a6cea4a9a15ce53343a79
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-10 15:02:14 +0100

    Revert styling

--- emoji-app/src/components/Item/Item.jsx
+++ emoji-app/src/components/Item/Item.jsx
@@ -2,35 +2,35 @@ import { useState } from "react";
 import styled from "styled-components";
 import { StyledButton } from "../styles/StyledButton";
 
-const StyledItem = styled.li`
-  background: salmon;
-  padding: 30px 50px;
-  border-radius: 20px;
-  text-align: center;
-  overflow: hidden;
-`;
+// const StyledItem = styled.li`
+//   background: salmon;
+//   padding: 30px 50px;
+//   border-radius: 20px;
+//   text-align: center;
+//   overflow: hidden;
+// `;
 
-const StyledEmoji = styled.span`
-  display: block;
-  margin-bottom: 42px;
-  font-size: 32px;
-  transition: transform 0.3s;
-  ${({ $zoomed }) => $zoomed && "transform: scale(2)"};
-`;
+// const StyledEmoji = styled.span`
+//   display: block;
+//   margin-bottom: 42px;
+//   font-size: 32px;
+//   transition: transform 0.3s;
+//   ${({ $zoomed }) => $zoomed && "transform: scale(2)"};
+// `;
 
 export function Item({ emoji, className }) {
   const [zoomed, setZoomed] = useState(false);
 
   return (
-    <StyledItem className={className}>
-      <StyledEmoji $zoomed={zoomed}>{emoji}</StyledEmoji>
-      <StyledButton
+    <li>
+      <span $zoomed={zoomed}>{emoji}</span>
+      <button
         onClick={() => {
           setZoomed((wasZoomed) => !wasZoomed);
         }}
       >
         {zoomed ? "Oddal" : "Przybliż"}
-      </StyledButton>
-    </StyledItem>
+      </button>
+    </li>
   );
 }
--- emoji-app/src/components/List/List.jsx
+++ emoji-app/src/components/List/List.jsx
@@ -1,36 +1,18 @@
 import { Item } from "../Item/Item";
-import styled from "styled-components";
-import { StyledButton } from "../styles/StyledButton";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
-
-const StyledList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  list-style: none;
-  gap: 30px;
-  padding: 30px;
-`;
-
-const StyledListItem = styled(Item)`
-  margin-bottom: 50px;
-`;
 
 export function List() {
   return (
     <>
-      <StyledButton
-        $margin={30}
-        $primary
-        onClick={() => alert(`Liczba emoji: ${emojis.length}`)}
-      >
+      <button onClick={() => alert(`Liczba emoji: ${emojis.length}`)}>
         Pokaż liczbę emoji!
-      </StyledButton>
-      <StyledList>
+      </button>
+      <ul>
         {emojis.map((emoji) => (
-          <StyledListItem key={emoji} emoji={emoji} />
+          <Item key={emoji} emoji={emoji} />
         ))}
-      </StyledList>
+      </ul>
     </>
   );
 }
