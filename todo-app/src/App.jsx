8e84e0ac72f908bf691c61ae2b9434108e478e46
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-10 19:02:07 +0100

    Set heading based on number of tasks

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -2,17 +2,31 @@ import styles from "./App.module.css";
 import { Form } from "./components/Form/Form";
 import { ToDoItem } from "./components/ToDoItem/ToDoItem";
 
+function getSubheading(numberOfTasks) {
+  switch (true) {
+    case numberOfTasks > 4:
+      return `${numberOfTasks} zadań`;
+    case numberOfTasks > 1:
+      return `${numberOfTasks} zadania`;
+    case numberOfTasks === 1:
+      return `${numberOfTasks} zadanie`;
+    default:
+      return `brak zadań`;
+  }
+}
+
 function App() {
   const todos = [
     { name: "Nauka Pythona", done: false, id: 1 },
     { name: "Wyzwanie AI", done: true, id: 2 },
   ];
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
         <div className="">
           <h1>Do zrobienia</h1>
-          <h2>5 zadań</h2>
+          <h2>{getSubheading(todos.length)}</h2>
         </div>
         <button className={styles.button}>+</button>
       </header>
