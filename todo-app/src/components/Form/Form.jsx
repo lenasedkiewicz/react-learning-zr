7b4dbcc17ab2d79c9c865075c9f6415cddad764b
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-10 18:35:23 +0100

    Add button; update rules

--- todo-app/src/components/Form/Form.jsx
+++ todo-app/src/components/Form/Form.jsx
@@ -1,10 +1,11 @@
+import { Button } from "../Button/Button";
 import styles from "./Form.module.css";
 
 export function Form() {
   return (
     <form className={styles.form}>
       <input className={styles.input} type="text" />
-      <button className={styles.button}>Dodaj</button>
+      <Button></Button>
     </form>
   );
 }
