bd6896be341dc7bc350ede79bec95041b7a5bca3
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-10 16:30:25 +0100

    Configure Tailwind

--- emoji-app/src/components/Item/Item.jsx
+++ emoji-app/src/components/Item/Item.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "tailwindcss";
 
 // const StyledItem = styled.li`
 //   background: salmon;
--- emoji-app/src/components/List/List.jsx
+++ emoji-app/src/components/List/List.jsx
@@ -1,5 +1,4 @@
 import { Item } from "../Item/Item";
-import "tailwindcss";
 
 const emojis = ["🚀", "🎶", "🎁", "🎉", "✨"];
 
