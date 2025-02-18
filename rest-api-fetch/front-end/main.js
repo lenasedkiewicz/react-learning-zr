d372dfe66d45538d8c2c9f0cfa610f8198726f95
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-18 19:05:08 +0100

    Start fetching data

--- rest-api-fetch/front-end/main.js
+++ rest-api-fetch/front-end/main.js
@@ -4,29 +4,35 @@ import "./style.css";
 const rootNode = document.querySelector("#app");
 
 const { renderShoppingList } = renderApp(
-    rootNode,
-    onDeleteItem,
-    onAddItem,
-    onEditItem
+  rootNode,
+  onDeleteItem,
+  onAddItem,
+  onEditItem
 );
 
 let list = [];
+
+fetch("http://localhost:3000/items").then((response) => {
+  if (response.ok) {
+    console.log(response.body);
+  }
+});
 renderShoppingList(list);
 
 function onDeleteItem(item) {
-    list = list.filter((i) => i !== item);
+  list = list.filter((i) => i !== item);
 
-    renderShoppingList(list);
+  renderShoppingList(list);
 }
 
 function onAddItem(item) {
-    list = [...list, item];
-    renderShoppingList(list);
+  list = [...list, item];
+  renderShoppingList(list);
 }
 
 function onEditItem(item) {
-    const index = list.lastIndexOf(item);
+  const index = list.lastIndexOf(item);
 
-    list[index] = item;
-    renderShoppingList(list);
+  list[index] = item;
+  renderShoppingList(list);
 }
