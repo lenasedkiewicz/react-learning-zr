7849c896c2381636459ab24a2c77b24fcf9cb0fb
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-20 17:21:48 +0100

    Fetch data from backend

--- revision-app-hooks-API/front-end/src/components/Panel/Panel.jsx
+++ revision-app-hooks-API/front-end/src/components/Panel/Panel.jsx
@@ -4,33 +4,24 @@ import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            word: "kot",
-            translation: "cat",
-            category: "noun",
-        },
-        {
-            id: 2,
-            word: "pies",
-            translation: "dog",
-            category: "noun",
-        },
-        {
-            id: 3,
-            word: "skakać",
-            translation: "jump",
-            category: "verb",
-        },
-    ]);
+  const [data, setData] = useState([]);
 
-    return (
-        <>
-            <Button>Załaduje dane</Button>
-            <section className={styles.section}>
-                <List data={data}></List>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <Button
+        onClick={() => {
+          fetch("http://localhost:3000/words")
+            .then((res) => res.json())
+            .then((res) => {
+              console.log(res);
+            });
+        }}
+      >
+        Załaduje dane
+      </Button>
+      <section className={styles.section}>
+        <List data={data}></List>
+      </section>
+    </>
+  );
 }
