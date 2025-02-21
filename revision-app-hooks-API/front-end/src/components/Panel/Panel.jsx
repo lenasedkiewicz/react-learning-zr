8c70e0e9c869a2ad2faed987262c560cc33b6f2c
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-21 16:56:33 +0100

    Add data adding

--- revision-app-hooks-API/front-end/src/components/Panel/Panel.jsx
+++ revision-app-hooks-API/front-end/src/components/Panel/Panel.jsx
@@ -16,7 +16,11 @@ export function Panel() {
   }, []);
 
   function handleFormSubmit(formData) {
-    console.log(formData);
+    fetch("http://localhost:3000/words", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
   }
 
   if (isLoading) {
