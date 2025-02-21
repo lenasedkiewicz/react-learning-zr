2afefaf6ee5fd4e35ddcb33f51b723ceeb906672
msedkiewicz <magdalenainspirations@gmail.com>
2025-02-21 14:26:00 +0100

    Add less troublesome data fetching with useEffect

--- revision-app-hooks-API/front-end/src/components/Panel/Panel.jsx
+++ revision-app-hooks-API/front-end/src/components/Panel/Panel.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../Button/Button";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
   const [data, setData] = useState([]);
-
-  function handleLoadClick() {
+  useEffect(() => {
     fetch("http://localhost:3000/words")
       .then((res) => res.json())
       .then((res) => {
         setData(res);
       });
-  }
+  }, []);
+
   return (
     <>
-      <Button onClick={handleLoadClick}>Załaduje dane</Button>
+      <Button>Załaduje dane</Button>
       <section className={styles.section}>
         <List data={data}></List>
       </section>
